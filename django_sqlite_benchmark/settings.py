5ec225d2d46d33f8c8f3b7a7e2b2c610807ca276
Simon Willison <swillison@gmail.com>
2022-10-20 14:24:34 -0700

    timeout: 20 for SQLite, refs #2

--- django_sqlite_benchmark/settings.py
+++ django_sqlite_benchmark/settings.py
@@ -78,6 +78,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "OPTIONS": {
+            "timeout": 20
+        }
     }
 }
 
