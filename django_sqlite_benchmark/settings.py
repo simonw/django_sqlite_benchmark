82ebbe1e82d9919f5eb821d499441d2b2a96518b
Simon Willison <swillison@gmail.com>
2022-10-21 19:38:25 -0700

    Bump timeout back to 20s, refs #3

--- django_sqlite_benchmark/settings.py
+++ django_sqlite_benchmark/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        # "OPTIONS": {
-        #     "timeout": 20
-        # }
+        "OPTIONS": {
+            "timeout": 20
+        }
     }
 }
 
